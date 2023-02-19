90bb387b567e07ffae255d72b0948c06a4462b85
kew2023 <58667109+kew2023@users.noreply.github.com>
2023-02-20 02:55:19 +0300

    1

---
+++ Лаб1/test.py
@@ -0,0 +1,11 @@
+from metods import *
+import wikipedia
+#print(BoyerMoore('12211212','12'))
+#print(predproc('21','12'))
+#print('1'.rfind('2'))
+#print(prefix('21'))
+#print(KMP('211221200','21'))
+print(KMP(['1','2','1','2'],('1','2')))
+#wikipedia.set_lang("ru")
+#origin = wikipedia.page("Рентгеновское излучение").content.lower()
+#print(origin[0])
